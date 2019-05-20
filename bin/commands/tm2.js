30652ec9bd422cdc5192ef819821fb98d9a5bcd8
Marc Farra <marcfarra@gmail.com>
2019-05-20 19:18:59 -0400

    endpoint should be required arg

--- bin/commands/tm2.js
+++ bin/commands/tm2.js
@@ -1,8 +1,8 @@
 const { TM } = require('../../lib')
 const pump = require('pump')
 
-function command (args, flags, context) {
-  const { endpoint } = flags
+function command (args, flags) {
+  const { endpoint } = args
   if (!endpoint) {
     console.error("Argument `endpoint` is required")
     return process.exit(1)
@@ -18,20 +18,22 @@ function command (args, flags, context) {
   })
 }
 
-const args = []
+const args = [
+  {
+    name: 'endpoint',
+    type: 'string',
+    description: 'The URL of the tasking manager',
+    required: 'true'
+  }
+]
 
 const flags = [
   {
-    name: 'endpoint',
-    alias: 'e',
-    type: 'string',
-  },
-  {
     name: 'proxy',
     alias: 'p',
+    description: 'Proxy URL in case of firewall',
     type: 'string'
-  },
-
+  }
 ]
 
 const options = {
@@ -39,7 +41,7 @@ const options = {
   longDescription: 'This command gets tasks from a Tasking Manager 2 compatible API.',
   examples: [
     {
-      cmd: 'overtask tm2 --endpoint http://tasks.openstreetmap.id',
+      cmd: 'overtask tm2 http://tasks.openstreetmap.id',
       description: 'Gets tasks from the Indonesia tasking manager'
     },
   ]
